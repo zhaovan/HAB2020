057f19a0d852722aec904b1754be73861844d522
Ivan <ivanzhao8@gmail.com>
2020-01-25 14:51:25 -0500

    Home Page setup

--- shopn/src/pages/HomePage.jsx
+++ shopn/src/pages/HomePage.jsx
@@ -1,11 +1,49 @@
 import React, { Component, Fragment } from "react";
-import { Container } from "@material-ui/core";
+import {
+  Container,
+  TextField,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button
+} from "@material-ui/core";
+
+import { MenuIcon } from "@material-ui/icons";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <Container maxWidth="sm"></Container>
+        <Container maxWidth="sm">
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                edge="start"
+                // className={classes.menuButton}
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6">News</Typography>
+              <Button color="inherit">Login</Button>
+            </Toolbar>
+          </AppBar>
+          <TextField
+            id="outlined-full-width"
+            // label="Search for a class!"
+            style={{ margin: 8 }}
+            placeholder="Search for a class"
+            // helperText="Full width!"
+            fullWidth
+            margin="normal"
+            variant="outlined"
+            InputLabelProps={{
+              shrink: true
+            }}
+          />
+        </Container>
       </Fragment>
     );
   }
