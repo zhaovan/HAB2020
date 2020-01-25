5e3fad0f6b29419832b775bf5b9a8c0d753b2ccc
SorinCho <sorincho@gmail.com>
2020-01-25 16:09:54 -0500

    Popup modal and multiple select

--- shopn/src/components/ComponentBar.jsx
+++ shopn/src/components/ComponentBar.jsx
@@ -6,6 +6,10 @@ import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
+import ReviewForm from "./ReviewForm";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -16,11 +20,31 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1
+  },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
   }
 }));
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div className={classes.root}>
@@ -37,7 +61,31 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Shopping Cart
           </Typography>
-          <Button color="inherit">Submit a Review</Button>
+          <Button color="inherit" onClick={handleOpen}>
+            Submit a Review
+          </Button>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">Review Form</h2>
+                <ReviewForm />
+                <p id="transition-modal-description">
+                  react-transition-group animates me.
+                </p>
+              </div>
+            </Fade>
+          </Modal>
         </Toolbar>
       </AppBar>
     </div>
---
+++ shopn/src/components/ReviewForm.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ShoppedSelect from "./ShoppedSelect";
+export default function ReviewForm() {
+  return (
+    <div>
+      <ShoppedSelect />
+    </div>
+  );
+}
