7f27b465370c8a4f227ca11147ed8454faeb2376
APikielny <apikielny@gmail.com>
2020-01-25 17:39:53 -0500

    update review page

--- shopn/src/App.js
+++ shopn/src/App.js
@@ -1,18 +1,25 @@
-import React from "react";
-import HomePage from "./pages/HomePage";
-import ClassPage from "./pages/ClassPage";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/class/" component={ClassPage} />
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- shopn/src/pages/HomePage.jsx
+++ shopn/src/pages/HomePage.jsx
@@ -1,30 +1,11 @@
 import React, { Component, Fragment } from "react";
-import { Container, TextField } from "@material-ui/core";
-import NavBar from "../components/ComponentBar";
-import GridComp from "../components/GridComp";
-
-import { db } from "../firebase";
+import { Container } from "@material-ui/core";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <NavBar />
-        <Container maxWidth="sm">
-          <TextField
-            id="outlined-full-width"
-            style={{ margin: 8 }}
-            placeholder="Search for a class"
-            fullWidth
-            margin="normal"
-            variant="outlined"
-            InputLabelProps={{
-              shrink: true
-            }}
-            style={{ margin: "50px 0px" }}
-          />
-          <GridComp />
-        </Container>
+        <Container maxWidth="sm"></Container>
       </Fragment>
     );
   }
