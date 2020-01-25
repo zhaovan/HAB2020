65b9849229df0212e811b369ff1ca9669d093970
Ivan <ivanzhao8@gmail.com>
2020-01-25 14:31:09 -0500

    npm i adds

---
+++ shopn/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import { Container } from "@material-ui/core";
+
+export class HomePage extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Container maxWidth="sm"></Container>
+      </Fragment>
+    );
+  }
+}
+
+export default HomePage;
