50bfed1e307f9f13c36e42a7140e26fbc89a4c10
Ivan <ivanzhao8@gmail.com>
2020-01-25 15:13:23 -0500

    add navbar

--- shopn/src/pages/HomePage.jsx
+++ shopn/src/pages/HomePage.jsx
@@ -1,43 +1,17 @@
 import React, { Component, Fragment } from "react";
-import {
-  Container,
-  TextField,
-  AppBar,
-  Toolbar,
-  IconButton,
-  Typography,
-  Button
-} from "@material-ui/core";
-
-import MenuIcon from "@material-ui/icons/Menu";
+import { Container, TextField } from "@material-ui/core";
+import NavBar from "../components/ComponentBar";
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <div style={{ flexGrow: 1 }}>
-          <AppBar position="static">
-            <Toolbar>
-              {/* <IconButton
-                edge="start"
-                // style={{marginRight: }}
-                color="inherit"
-                aria-label="menu"
-              >
-                <MenuIcon />
-              </IconButton> */}
-              <Typography variant="h6">Shopping Cart</Typography>
-              <Button color="inherit">Add a review</Button>
-            </Toolbar>
-          </AppBar>
-        </div>
+        <NavBar />
         <Container maxWidth="sm">
           <TextField
             id="outlined-full-width"
-            // label="Search for a class!"
             style={{ margin: 8 }}
             placeholder="Search for a class"
-            // helperText="Full width!"
             fullWidth
             margin="normal"
             variant="outlined"
