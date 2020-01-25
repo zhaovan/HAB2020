45edb1e7ff3f9dba421a2cf07a96f9190f937e2b
SorinCho <sorincho@gmail.com>
2020-01-25 16:38:17 -0500

    Conditional view for took and shop

--- shopn/src/components/ReviewForm.jsx
+++ shopn/src/components/ReviewForm.jsx
@@ -1,9 +1,78 @@
 import React from "react";
 import ShoppedSelect from "./ShoppedSelect";
-export default function ReviewForm() {
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import ShopForm from "./ShopForm";
+import TookForm from "./TookForm";
+
+function ReviewRadio() {
+  const [took, setTook] = React.useState("false");
+
+  const handleChange = event => {
+    setTook(event.target.value);
+  };
   return (
-    <div>
-      <ShoppedSelect />
-    </div>
+    <FormControl component="fieldset">
+      <FormLabel component="legend">labelPlacement</FormLabel>
+      <RadioGroup
+        aria-label="position"
+        name="position"
+        value={took}
+        onChange={handleChange}
+        row
+      >
+        <FormControlLabel
+          value="false"
+          control={<Radio color="primary" />}
+          label="Shopping"
+          labelPlacement="end"
+        />
+        <FormControlLabel
+          value="true"
+          control={<Radio color="primary" />}
+          label="Took"
+          labelPlacement="end"
+        />
+      </RadioGroup>
+    </FormControl>
+  );
+}
+
+export default function ReviewForm() {
+  const [took, setTook] = React.useState("false");
+
+  const handleChange = event => {
+    setTook(event.target.value);
+  };
+  return (
+    <>
+      <FormControl component="fieldset">
+        <FormLabel component="legend">labelPlacement</FormLabel>
+        <RadioGroup
+          aria-label="position"
+          name="position"
+          value={took}
+          onChange={handleChange}
+          row
+        >
+          <FormControlLabel
+            value="false"
+            control={<Radio color="primary" />}
+            label="Shopping"
+            labelPlacement="end"
+          />
+          <FormControlLabel
+            value="true"
+            control={<Radio color="primary" />}
+            label="Took"
+            labelPlacement="end"
+          />
+        </RadioGroup>
+      </FormControl>
+      <div>{took === "true" ? <TookForm /> : <ShopForm />}</div>
+    </>
   );
 }
---
+++ shopn/src/components/ShopForm.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ShoppedSelect from "./ShoppedSelect";
+
+export default function ShopForm() {
+  return (
+    <div>
+      <h2>Shop</h2>
+      <ShoppedSelect />
+    </div>
+  );
+}
